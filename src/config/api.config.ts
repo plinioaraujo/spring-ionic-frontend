7c3e6f68bce0f29e62f62d9353076e3dd7c0815f
plinioaraujo <plinioglad@gmail.com>
2018-09-25 01:28:34 -0300

    Mostrando dados e imagem do cliente para pagina de profile

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,8 @@
 export  const API_CONFIG = {
     base_url: 'http://localhost:8085',
-    bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/spring-ionic-pa'
+    _bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/spring-ionic-pa',
+    get bucketBaseUrl() {
+        return this._bucketBaseUrl;
+    },
+    
 }
